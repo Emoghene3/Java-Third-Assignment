457f493756c2a647561ded3237052f8d917a7e44
emoghene3 <oghenekohwarhoemoghene@gmail.com>
2022-05-09 07:04:10 -0700

    first commit

---
+++ index.js
@@ -0,0 +1,41 @@
+
+
+
+
+
+// Assignment 2 Print out the name of students in a particular department
+
+const nameOfStudents = ["Rose", "Amaka", "Gabriel", "John", "Andrew", "Mercy", "Tolu", "Marvelous", "Mamus", "Ruth", "Sunny", "Victory", "Coliins", "Tomi", "Victor", "Jude",]
+
+for(let h = 0; h < nameOfStudents.length; h++) 
+    
+    console.log(nameOfStudents[h])
+
+    
+
+
+
+
+
+
+
+
+
+
+
+// Assignment 3 Calculate using pi * r**2 (where pi=3.142 r= )
+
+const pi = 3.142
+const r = 5
+
+// circumference of a circle
+
+C = 2 * pi * 5 
+
+console.log(C)
+
+//  Area of a circle
+
+A = pi * r**2
+
+console.log(A)
